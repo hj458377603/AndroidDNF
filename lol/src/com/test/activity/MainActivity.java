2bc892f59412ff0d3f442418ce4a3eca90d115ef
locas <hj458377603@hotmail.com>
2014-11-03 18:08:41 +0800

    jun:title_bar

---
+++ lol/src/com/test/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.test.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.test.lol.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+}
